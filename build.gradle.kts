9b6480835acf9ff32d2267c80738befbff640ca2
XXMD <xxmd3720@gmail.com>
2024-07-18 18:44:59 +0800

    修改icon，version1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "1.0"
-            versionCode = 1
+            versionName = "1.1"
+            versionCode = 1.1
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
